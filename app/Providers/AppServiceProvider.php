38e6562e0bfe30ba7f14962c6e1701d6802e2fc2
sarage92 <inara97_97@mail.ru>
2019-01-29 15:06:26 +0400

    Rename public to public_html

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,5 +26,8 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         //
+        $this->app->bind('path.public', function () {
+            return base_path() . '/public_html';
+        });
     }
 }
