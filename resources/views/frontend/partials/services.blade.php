d85634494726065ab3784315ec9de2200d41493e
saarge92 <sarage92@mail.ru>
2019-05-04 13:55:54 +0400

    Redesign sevice list

--- resources/views/frontend/partials/services.blade.php
+++ resources/views/frontend/partials/services.blade.php
@@ -12,26 +12,27 @@
         <div class="row text-center">
             <div class="services-contents">
                 @foreach ($chunkedServices as $service)
-                <div class="col-md-4 col-sm-4 col-xs-12">
+                <div class="col-md-4 col-sm-4 col-xs-12" style="padding-bottom:1.5rem;">
                     <div class="about-move">
                         <div class="services-details">
                             <div class="single-services">
                                 <a class="services-icon" href="#">
-                                <img src="{{Storage::url($service->path)}}" class="service-img">
+                                    <img src="{{Storage::url($service->path)}}" class="service-img">
                                 </a>
                                 <h4>{{$service->title}}</h4>
-                                <div class="text-justify">
+                                <div class="text-justify" style="height:8.5em;">
                                     {{$service->content}}
                                 </div>
                             </div>
+                            <div class="text-center price_info mt-1 mb-1">
+                                Цена {{$service->price}} р
+                            </div>
+                            <div class="text-center" style="padding-bottom:1.2rem;">
+                                <button data-service_id="{{$service->id}}" data-toggle="modal" data-target="#infoModal"
+                                    class="orderService btn btn-danger">Заказать</button>
+                            </div>
                         </div>
                         <!-- end about-details -->
-                    </div>
-                    <div class="text-center price_info mt-1 mb-1">
-                        Цена {{$service->price}} р
-                    </div>
-                    <div class="text-center">
-                        <button data-service_id="{{$service->id}}" data-toggle="modal" data-target="#infoModal" class="orderService btn btn-danger">Заказать</button>
                     </div>
                 </div>
                 @endforeach
@@ -39,7 +40,7 @@
         </div>
         @endforeach
         <div class="text-center">
-        <a href="{{route('frontend.services')}}" class="btn btn-primary">Все услуги</a>
+            <a href="{{route('frontend.services')}}" class="btn btn-primary" style="margin-top:1.7rem;">Все услуги</a>
         </div>
     </div>
 </div>
